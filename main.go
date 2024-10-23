087117ae4a739ce619df7fb646e573581b2c1f27
jorniklenderlyn <khcfksiirjfldik@gmail.com>
2024-10-23 18:56:27 +1000

    beta calc

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"errors"
-	"fmt"
+	// "fmt"
 	"strconv"
 	"strings"
 )
@@ -19,7 +19,50 @@ func FindRune(str string, r rune, start int, step int) int {
 	return i
 }
 
+func ProcessNum(n string) (float64, error) {
+	rarr := []rune(n)
+	cur_num := ""
+	var ret float64 = 0
+	var sign rune = 0
+	for _, el := range rarr {
+		if el == '*' || el == '/' {
+			fnum, ok := strconv.ParseFloat(cur_num, 64)
+			if ok != nil {
+				return 0, errors.New("bad num format1")
+			}
+			if ret == 0 {
+				ret = fnum
+			} else {
+				if sign == '*' {
+					ret *= fnum
+				} else {
+					ret /= fnum
+				}
+			}
+			sign = el
+			cur_num = ""
+		} else {
+			cur_num += string(el)
+		}
+	}
+	if cur_num != "" {
+		fnum, ok := strconv.ParseFloat(cur_num, 64)
+		if ok != nil {
+			return 0, errors.New("bad num format2")
+		}
+		if sign == '*' {
+			ret *= fnum
+		} else if sign == 0 {
+			ret = fnum
+		} else {
+			ret /= fnum
+		}
+	}
+	return ret, nil
+}
+
 func Calc(expression string) (float64, error) {
+	// fmt.Println("S", expression)
 	expression = strings.Replace(expression, " ", "", -1)
 	rarr := []rune(expression)
 	// preprocess
@@ -40,21 +83,71 @@ func Calc(expression string) (float64, error) {
 					return num, ok
 				}
 				new_exp += strconv.FormatFloat(num, 'f', -1, 64)
-				continue
+				// continue
 			}
 			if bracket_depth == 0 {
-				return 0, errors.New("bad brackets seq")
+				return 0, errors.New("bad brackets seq1")
 			}
 			bracket_depth--
 		} else if bracket_depth == 0 {
 			new_exp += string(rarr[i])
 		}
 	}
-	fmt.Println(new_exp)
-	return 0, nil
+	if bracket_depth != 0 {
+		// fmt.Println(bracket_depth)
+		return 0, errors.New("bad brackets seq2")
+	}
+	rarr = []rune(new_exp)
+	// narr := []int{}
+	// cur_num := ""
+	// ret := 0
+	// cur_num := 1
+	// a := 1
+	// for i := 0; i < len(new_exp); i++ {
+	// 	if rarr[i] == '+' || rarr[i] == '-' {
+	// 		strconv.
+	// 		if rarr == '-' {
+	// 			a = -1
+	// 		} else {
+	// 			a = 1
+	// 		}
+	// 	}
+	// }
+	// if cur_num != "" {
+	// 	ret +
+	// }
+	signs := []rune{}
+	for _, el := range rarr {
+		if el == '+' || el == '-' {
+			signs = append(signs, el)
+		}
+	}
+	new_exp = strings.Replace(new_exp, "-", "+", -1)
+	nums := strings.Split(new_exp, "+")
+	fnums := []float64{}
+	for _, n := range nums {
+		fnum, ok := ProcessNum(n)
+		if ok != nil {
+			return 0, ok
+		}
+		fnums = append(fnums, fnum)
+	}
+	ret := fnums[0]
+	for i := 1; i < len(fnums); i++ {
+		if signs[i-1] == '+' {
+			ret += fnums[i]
+		} else {
+			ret -= fnums[i]
+		}
+	}
+	// fmt.Println(signs)
+	// fmt.Println(fnums)
+	// fmt.Println(new_exp)
+	return ret, nil
 }
 
-func main() {
-	// fmt.Print(FindRune("()", '(', 1, -1))
-	fmt.Print(Calc("1 + 3 + 4(1)"))
-}
+// func main() {
+// fmt.Print(FindRune("()", '(', 1, -1))
+// fmt.Print(Calc("(1 + 3 + 4*2) / 12"))
+// fmt.Print(ProcessNum("1"))
+// }
