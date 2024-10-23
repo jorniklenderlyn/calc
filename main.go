101308da3b1c2d82c7d47f22e333702f85943c38
jorniklenderlyn <khcfksiirjfldik@gmail.com>
2024-10-23 19:31:37 +1000

    final calc

--- main.go
+++ main.go
@@ -2,24 +2,17 @@ package main
 
 import (
 	"errors"
-	// "fmt"
 	"strconv"
 	"strings"
 )
 
-func FindRune(str string, r rune, start int, step int) int {
-	rarr := []rune(str)
-	i := start
-	for i > -1 && i < len(str) && rarr[i] != r {
-		i += step
-	}
-	if i >= len(str) {
-		i = -1
-	}
-	return i
-}
-
 func ProcessNum(n string) (float64, error) {
+	if strings.Replace(n, " ", "", -1) == "" {
+		return 0, errors.New("bad num format1")
+	}
+	if n[len(n)-1] == '*' || n[len(n)-1] == '/' {
+		return 0, errors.New("bad num format1")
+	}
 	rarr := []rune(n)
 	cur_num := ""
 	var ret float64 = 0
@@ -62,10 +55,8 @@ func ProcessNum(n string) (float64, error) {
 }
 
 func Calc(expression string) (float64, error) {
-	// fmt.Println("S", expression)
 	expression = strings.Replace(expression, " ", "", -1)
 	rarr := []rune(expression)
-	// preprocess
 	bracket_depth := 0
 	l := 0
 	// r := 0;
@@ -83,7 +74,6 @@ func Calc(expression string) (float64, error) {
 					return num, ok
 				}
 				new_exp += strconv.FormatFloat(num, 'f', -1, 64)
-				// continue
 			}
 			if bracket_depth == 0 {
 				return 0, errors.New("bad brackets seq1")
@@ -94,28 +84,9 @@ func Calc(expression string) (float64, error) {
 		}
 	}
 	if bracket_depth != 0 {
-		// fmt.Println(bracket_depth)
 		return 0, errors.New("bad brackets seq2")
 	}
 	rarr = []rune(new_exp)
-	// narr := []int{}
-	// cur_num := ""
-	// ret := 0
-	// cur_num := 1
-	// a := 1
-	// for i := 0; i < len(new_exp); i++ {
-	// 	if rarr[i] == '+' || rarr[i] == '-' {
-	// 		strconv.
-	// 		if rarr == '-' {
-	// 			a = -1
-	// 		} else {
-	// 			a = 1
-	// 		}
-	// 	}
-	// }
-	// if cur_num != "" {
-	// 	ret +
-	// }
 	signs := []rune{}
 	for _, el := range rarr {
 		if el == '+' || el == '-' {
@@ -140,14 +111,5 @@ func Calc(expression string) (float64, error) {
 			ret -= fnums[i]
 		}
 	}
-	// fmt.Println(signs)
-	// fmt.Println(fnums)
-	// fmt.Println(new_exp)
 	return ret, nil
 }
-
-// func main() {
-// fmt.Print(FindRune("()", '(', 1, -1))
-// fmt.Print(Calc("(1 + 3 + 4*2) / 12"))
-// fmt.Print(ProcessNum("1"))
-// }
